0953a122fe17a26397377f02348e544e32d37338
Clint Banzhaf <clint@banzhaf.pro>
2024-07-26 14:09:55 +0200

    update protocols.m

--- components/protocols.m
+++ components/protocols.m
@@ -11,9 +11,6 @@ classdef protocols < handle
     end
     
     methods
-        function self = protocols()
-        end
-        
         function reload(self, device)
             self.list = [];
             self.selected = struct();
@@ -22,8 +19,8 @@ classdef protocols < handle
                 file = files(f, 1:end);
                 name = strtrim(erase(file, ".m"));
                 fh = feval(name);
-                req = fh.requires();          
-                if req.devicetype ~= "ANY" && req.devicetype ~= device.type
+                req = fh.requires();
+                if ~self.iscompatible(req, device)
                     continue
                 end
                 idx = length(self.list) + 1;
@@ -31,6 +28,29 @@ classdef protocols < handle
                 self.list(idx).fh = fh;
                 self.list(idx).req = req;
             end
+        end
+        
+        function r = iscompatible(~, req, dev)
+            % check device type
+            if req.devicetype ~= "ANY" && req.devicetype ~= dev.type
+                r = false;
+                return
+            end
+            % check channel requirements
+            for idx = 1:length(req.channels)
+                found = 0;
+                for lslch = 1:length(dev.lsl.channels)
+                    if req.channels(idx).type == dev.lsl.channels(lslch).type && ...
+                       req.channels(idx).unit == dev.lsl.channels(lslch).unit
+                       found = found + 1;
+                    end
+                end
+                if found < req.channels(idx).min
+                    r = false;
+                    return
+                end
+            end
+            r = true;
         end
         
         function r = select(self, name)
